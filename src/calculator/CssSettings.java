eaefa53605e0f1d78b54bb73b79f758e25782d38
wantczak <antek10001@gmail.com>
2016-04-19 19:29:27 +0200

    Final commit

--- src/calculator/CssSettings.java
+++ src/calculator/CssSettings.java
@@ -1,7 +1,8 @@
 package calculator;
 
 public class CssSettings {
-	private String font;
+	private String font = "Arial";
+	private String fontStyle = "normal";
 
 	public String getFont() {
 		return font;
@@ -9,5 +10,13 @@ public class CssSettings {
 
 	public void setFont(String font) {
 		this.font = font;
+	}
+
+	public String getFontStyle() {
+		return fontStyle;
+	}
+
+	public void setFontStyle(String fontStyle) {
+		this.fontStyle = fontStyle;
 	}
 }
